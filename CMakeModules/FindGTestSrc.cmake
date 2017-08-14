820bf254a90fd74c32c7f86aef40c6e03feea11a
lilinj2000 <lilinj2000@gmail.com>
2017-08-14 07:01:27 +0000

    update for gtest supported.

--- CMakeModules/FindGTestSrc.cmake
+++ CMakeModules/FindGTestSrc.cmake
@@ -1,7 +1,8 @@
 
 SET(GTEST_SEARCH_PATH
     "${GTEST_SOURCE_DIR}"
-    "${CMAKE_CURRENT_LIST_DIR}/../thirdparty/gtest/googletest")
+    "${CMAKE_CURRENT_LIST_DIR}/../thirdparty/gtest/googletest"
+    "~/3rd/googletest")
 
 IF(UNIX)
     IF(RAPIDJSON_BUILD_THIRDPARTY_GTEST)
@@ -11,9 +12,9 @@ IF(UNIX)
     ENDIF()
 ENDIF()
 
-FIND_PATH(GTEST_SOURCE_DIR
-    NAMES CMakeLists.txt src/gtest_main.cc
-    PATHS ${GTEST_SEARCH_PATH})
+# FIND_PATH(GTEST_SOURCE_DIR
+#     NAMES CMakeLists.txt src/gtest_main.cc
+#     PATHS ${GTEST_SEARCH_PATH})
 
 
 # Debian installs gtest include directory in /usr/include, thus need to look
@@ -21,10 +22,16 @@ FIND_PATH(GTEST_SOURCE_DIR
 FIND_PATH(GTEST_INCLUDE_DIR
     NAMES gtest/gtest.h
     PATH_SUFFIXES include
-    HINTS ${GTEST_SOURCE_DIR}
+    # HINTS ${GTEST_SOURCE_DIR}
     PATHS ${GTEST_SEARCH_PATH})
 
-INCLUDE(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GTestSrc DEFAULT_MSG
-    GTEST_SOURCE_DIR
-    GTEST_INCLUDE_DIR)
+FIND_PATH(GTEST_LIB_DIR
+    NAMES libgtest.a libgtest_main.a
+    PATH_SUFFIXES lib
+    # HINTS ${GTEST_SOURCE_DIR}
+    PATHS ${GTEST_SEARCH_PATH})
+
+# INCLUDE(FindPackageHandleStandardArgs)
+# find_package_handle_standard_args(GTestSrc DEFAULT_MSG
+#     GTEST_SOURCE_DIR
+#     GTEST_INCLUDE_DIR)
